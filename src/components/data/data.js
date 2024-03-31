eaa273cb709220d1cab6b7b9df2ce33909b8c0a2
tradervicky <vickygupta031@gmail.com>
2024-03-31 19:41:01 +0530

    text deleted

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -15,20 +15,20 @@ export const data = [
         name: "Vicky Gupta",
         Place : "Mathiya, Bihar",
         image: user_2,
-        desc: "Choosing HeartCare was the best decision I made for my healthcare. The staff went above and beyond to ensure my comfort and well-being. The facilities were clean and modern, and the entire team worked seamlessly together to provide me with the care I needed. I would highly recommend HeartCare to anyone seeking top-notch medical treatment."
+        desc: "Choosing HeartCare was the best decision I made for my healthcare. The staff went above and beyond to ensure my comfort and well-being. The facilities were clean and modern, and the entire team worked together to provide me with the care. I would highly recommend HeartCare to anyone seeking medical treatment."
     },
     {
         id:3,
         name: "Nora Kumari",
         Place : "Utah, Indonesia",
         image: user_3,
-        desc: "I am forever grateful to the team at HeartCare for their exceptional care during my recent surgery. From the pre-operative consultations to the post-operative follow-ups, they were with me every step of the way. Their expertise and dedication gave me confidence throughout the process, and I couldn't be happier with the results. Thank you, HeartCare, for truly making a difference in my life."
+        desc: "I am forever grateful to the team at HeartCare for their exceptional care during my recent surgery.They were with me every step of the way. Their expertise and dedication gave me confidence throughout the process, and I couldn't be happier with the results. Thank you, HeartCare, for truly making a difference in my life."
     },
     {
         id:4,
         name: "Sandeep Kumar",
         Place : "Nandpur, Siwan",
         image: user_4,
-        desc: "As someone who has been managing a chronic condition for years, finding a healthcare provider I trust is paramount. HeartCare exceeded all my expectations. The specialists took the time to understand my unique needs and develop a personalized treatment plan that has greatly improved my quality of life. The entire staff at HeartCare is truly exceptional, and I wouldn't hesitate to recommend them to anyone in need of compassionate, expert care."
+        desc: "As someone who has been managing a chronic condition for years, finding a healthcare provider I trust is paramount. HeartCare exceeded all my expectations. The specialists took the time to understand my unique needs and develop a personalized treatment plan that has greatly improved my quality of life."
     },
 ]
