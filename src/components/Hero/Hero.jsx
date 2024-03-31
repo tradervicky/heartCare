8bb815cc2449cb9d009f7619ee65bb7429ca1ae8
tradervicky <vickygupta031@gmail.com>
2024-03-31 10:07:11 +0530

    scroll added in hero section also

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,15 +3,20 @@ import './Hero.css'
 import bg from '../../assets/bg_1.jpg'
 import Button from '../button/Button'
 import icon_arrow from '../../assets/dark-arrow.png'
+import { scrollToSection } from '../utils/ScrollUtils'
 const Hero = () => {
-    
+  const handleNavLinkClick = (sectionId) => {
+    scrollToSection(sectionId);
+  }
+
   return (
    <div className='hero  '>
     <div className="max-w-[800px] flex flex-col justify-center items-center mobile:mx-4">
     <h1 className='text-4xl font-bold text-center mobile:text-2xl mobile:font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, nisi?</h1>
     <p className='text-center text-md pt-4 font-semibold mobile:text-sm mobile:font-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero blanditiis, velit dolorem sint, eligendi, tenetur beatae laudantium perferendis distinctio voluptas odio aliquid rem consectetur delectus.</p>
     <div className='py-4'>
-    <Button text="Explore more" icon={icon_arrow} customCss="mobile:px-2 mobile:py-1 mobile:text-md"/>
+    <Button text="Explore more" icon={icon_arrow} customCss="mobile:px-2 mobile:py-1 mobile:text-md" 
+    onClick={()=>handleNavLinkClick('section6')}/>
 
     </div>
     </div>
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({text, customCss, onClick, icon}) => {
     
   return (
     <>
-    <button className={`bg-white px-4 py-2 font-semibold rounded-3xl text-blue-950 border-2 flex items-center hover:bg-blue-200  duration-300 ${customCss}`}>{text}{icon ? <img className='w-8 pl-2' src={icon} alt="" />: ""}</button>
+    <button onClick={onClick} className={`bg-white px-4 py-2 font-semibold rounded-3xl text-blue-950 border-2 flex items-center hover:bg-blue-200  duration-300 ${customCss}`}>{text}{icon ? <img className='w-8 pl-2' src={icon} alt="" />: ""}</button>
     </>
   )
 }
